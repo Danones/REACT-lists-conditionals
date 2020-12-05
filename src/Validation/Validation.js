9d0a0dc3654bf45c43a2bc9b8a26fa4b200fc1bd
Danones <diogo.sousa.uk@gmail.com>
2020-12-05 19:34:41 +0000

    New method to delete characters

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 const validation = props => {
+  let validationMessage = "Text too short";
+
+  if (props.inputLength > 5) {
+    validationMessage = "Text long enough";
+  }
   return (
     <div>
-      {props.inputLength < 5 ? <p>Text too short</p> : <p>Text long enough</p>}
+      <p>{validationMessage}</p>
     </div>
   );
 };
